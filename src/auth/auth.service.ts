e9a0bf257fa21c05e6a595815fcb09ea2e097275
React Mastering <pga.adam.scott@gmail.com>
2022-08-05 19:16:36 +0900

    init

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -54,7 +54,7 @@ export class AuthService {
     };
     const secret = this.config.get('JWT_SECRET');
     const token = await this.jwt.signAsync(payload, {
-      expiresIn: '1m',
+      expiresIn: '5m',
       secret: secret,
     });
     return {
